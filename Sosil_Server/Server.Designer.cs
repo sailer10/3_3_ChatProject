59476c8236b26ad799a0acf93144f41c9c51ff1b
sailer10 <80940663+sailer10@users.noreply.github.com>
2023-06-14 16:16:19 +0900

    server file added

---
+++ Sosil_Server/Server.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace Sosil_Server
+{
+    partial class Server
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Server));
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.btn1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblStartTime = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lsbClients = new System.Windows.Forms.ListBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.btnSchedule = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtLog
+            // 
+            this.txtLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtLog.Font = new System.Drawing.Font("굴림", 10F);
+            this.txtLog.Location = new System.Drawing.Point(0, 0);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(469, 601);
+            this.txtLog.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnStart.Location = new System.Drawing.Point(0, 545);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(226, 56);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "서버 시작";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "default_image.png");
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(25, 12);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(169, 42);
+            this.btn1.TabIndex = 6;
+            this.btn1.Text = "가입자 목록";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 488);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 15);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "서버 실행시간 :";
+            // 
+            // lblStartTime
+            // 
+            this.lblStartTime.AutoSize = true;
+            this.lblStartTime.Location = new System.Drawing.Point(122, 488);
+            this.lblStartTime.Name = "lblStartTime";
+            this.lblStartTime.Size = new System.Drawing.Size(15, 15);
+            this.lblStartTime.TabIndex = 9;
+            this.lblStartTime.Text = "0";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(25, 370);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(169, 36);
+            this.btnClear.TabIndex = 10;
+            this.btnClear.Text = "Clear Log";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // lsbClients
+            // 
+            this.lsbClients.FormattingEnabled = true;
+            this.lsbClients.ItemHeight = 15;
+            this.lsbClients.Location = new System.Drawing.Point(25, 130);
+            this.lsbClients.Name = "lsbClients";
+            this.lsbClients.Size = new System.Drawing.Size(169, 64);
+            this.lsbClients.TabIndex = 11;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(25, 231);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(169, 33);
+            this.btnSend.TabIndex = 12;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtSend
+            // 
+            this.txtSend.Location = new System.Drawing.Point(25, 200);
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(169, 25);
+            this.txtSend.TabIndex = 13;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(25, 298);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(169, 30);
+            this.btnSave.TabIndex = 14;
+            this.btnSave.Text = "클라이언트 목록 저장";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 459);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 15);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "현재 시각 :";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(91, 459);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(15, 15);
+            this.lblTime.TabIndex = 16;
+            this.lblTime.Text = "0";
+            // 
+            // btnSchedule
+            // 
+            this.btnSchedule.Location = new System.Drawing.Point(25, 60);
+            this.btnSchedule.Name = "btnSchedule";
+            this.btnSchedule.Size = new System.Drawing.Size(169, 33);
+            this.btnSchedule.TabIndex = 17;
+            this.btnSchedule.Text = "일정 목록";
+            this.btnSchedule.UseVisualStyleBackColor = true;
+            this.btnSchedule.Click += new System.EventHandler(this.btnSchedule_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btn1);
+            this.panel1.Controls.Add(this.btnSave);
+            this.panel1.Controls.Add(this.lblTime);
+            this.panel1.Controls.Add(this.btnClear);
+            this.panel1.Controls.Add(this.btnSchedule);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.lsbClients);
+            this.panel1.Controls.Add(this.txtSend);
+            this.panel1.Controls.Add(this.btnSend);
+            this.panel1.Controls.Add(this.lblStartTime);
+            this.panel1.Controls.Add(this.btnStart);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(469, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(226, 601);
+            this.panel1.TabIndex = 18;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.txtLog);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(469, 601);
+            this.panel2.TabIndex = 19;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Server
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(695, 601);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Server";
+            this.Text = "Server";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Server_FormClosing);
+            this.Load += new System.EventHandler(this.Server_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblStartTime;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.ListBox lsbClients;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Button btnSchedule;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
